892bdb415331775a1e19787d2cfaf36ba9632adb
bankarom <ombankar5678@gmail.com>
2024-03-26 10:24:20 +0530

    module12

--- Server/routes.js
+++ Server/routes.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const car = require('./schema');
 
-router.use(express.json())
+router.use(express.json());
 
-router.get('/get',(req,res)=>{
-    res.send('get request')
-})
+router.get('/get', async (req, res) => {
+    try {
+        const sports = await car.find();
+        res.json(sports);
+    } catch (err) {
+        console.error('error', err);
+        res.status(500).json({ error: 'internal server error' });
+    }
+});
 
-router.post('/post',(req,res)=>{
-    console.log(req.body)
-    res.json(req.body)
-})
+router.post('/post', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
 
-router.put('/put',(req,res)=>{
-    res.send("put request")
-})
+router.put('/put', (req, res) => {
+    res.send("put request");
+});
 
-router.delete('/delete',(req,res)=>{
-    res.send("delete request")
-})
+router.delete('/delete', (req, res) => {
+    res.send("delete request");
+});
 
-module.exports = router
+module.exports = router;
